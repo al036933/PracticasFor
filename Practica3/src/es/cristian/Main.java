ec797396b7c43e06e8d591121e46b3b490392d99
Cristian García Hernández <al036933@ieslaencanta.com>
2022-02-04 13:48:07 +0100

    Practica 3 haciendo

--- Practica3/src/es/cristian/Main.java
+++ Practica3/src/es/cristian/Main.java
@@ -5,16 +5,21 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int i;
-        int numero;
+        int numero, numeroDeDivisores = 0;
 
         Scanner entrada = new Scanner(System.in);
 
         System.out.print("Escribe un número para comprobar si es primo: ");
         numero = entrada.nextInt();
 
-        for (i = 2; i < numero; i++) {
+        for (int i = 2; i < numero; i++) {
 
+        }
 
+        if (numeroDeDivisores == 0) {
+            System.out.println("El número " + numero + " es primo");
+        } else {
+            System.out.println("El número " + numero + " no es primo");
+        }
     }
 }
